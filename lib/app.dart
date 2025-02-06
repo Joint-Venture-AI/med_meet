234ef9ef2b7bf1b646a4ad885b7df38b6a5d01bd
Zimmer550i <sakif049@gmail.com>
2025-02-06 12:41:04 +0600

    fix: app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:med_meet_flutter/core/components/bottom_navigation.dart';
 import 'package:med_meet_flutter/views/home/user/home_user.dart';
+import 'package:med_meet_flutter/views/profile/profile_view.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -10,26 +11,34 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  PageController controller = PageController();
   int _index = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
+        controller: controller,
+        onPageChanged: (value) {
+          setState(() {
+            _index = value;
+          });
+        },
         children: [
           HomeUserView(),
           HomeUserView(),
           HomeUserView(),
-          HomeUserView(),
-          HomeUserView(),
+          ProfileView(),
         ],
       ),
       bottomNavigationBar: BottomNavigation(
         menuIndex: _index,
         onChanged: (index) {
-          setState(() {
-            _index = index;
-          });
+          controller.animateToPage(
+            index,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.decelerate,
+          );
         },
       ),
     );
