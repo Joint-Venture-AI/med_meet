43d92e018c9cb49b015d16f64a033994c48e74f8
Zimmer550i <sakif049@gmail.com>
2025-02-07 09:48:00 +0600

    add: delete account button action

--- lib/views/profile/settings.dart
+++ lib/views/profile/settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:med_meet_flutter/core/components/custom_app_bar.dart';
+import 'package:med_meet_flutter/core/components/custom_button.dart';
 import 'package:med_meet_flutter/core/helpers/route.dart';
 import 'package:med_meet_flutter/core/utils/uitls.dart';
 import 'package:med_meet_flutter/models/options.dart';
@@ -61,6 +62,95 @@ class Settings extends StatelessWidget {
                           onTap: () {
                             if (e.link != null) {
                               Get.toNamed(e.link!);
+                            } else if (e.name == "Delete Account") {
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return Dialog(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                    child: Container(
+                                      // width: MediaQuery.of(context).size.width -
+                                      //     48,
+                                      padding: EdgeInsets.only(
+                                        top: 8,
+                                      ),
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(20),
+                                        border: Border.all(
+                                          color: Color(0xffE8E8E8),
+                                        ),
+                                      ),
+                                      child: Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Container(
+                                            height: 3,
+                                            width: 40,
+                                            color: Color(0xffE8E8E8),
+                                          ),
+                                          SizedBox(height: 12),
+                                          Text(
+                                            "Delete Account",
+                                            style: TextStyle(
+                                              fontSize: 20,
+                                              color: Color(0xffDD3663),
+                                            ),
+                                          ),
+                                          SizedBox(height: 12),
+                                          Divider(
+                                            color: Color(0xffDDDEE0),
+                                          ),
+                                          SizedBox(height: 12),
+                                          Padding(
+                                            padding: const EdgeInsets.symmetric(
+                                              horizontal: 24,
+                                            ),
+                                            child: Text(
+                                              "Are you sure you want to delete your Account?",
+                                              textAlign: TextAlign.center,
+                                              style: TextStyle(
+                                                fontSize: 18,
+                                                color: Color(0xff545454),
+                                              ),
+                                            ),
+                                          ),
+                                          SizedBox(height: 12),
+                                          Row(
+                                            children: [
+                                              const SizedBox(width: 12),
+                                              Expanded(
+                                                child: CustomButton(
+                                                  onPressed: () {
+                                                    Get.back();
+                                                  },
+                                                  buttonTitle: "Cancel",
+                                                  height: 48,
+                                                  isSecondary: true,
+                                                ),
+                                              ),
+                                              const SizedBox(width: 12),
+                                              Expanded(
+                                                child: CustomButton(
+                                                  onPressed: () {
+                                                    Get.back();
+                                                  },
+                                                  buttonTitle: "Yes, Delete",
+                                                  height: 48,
+                                                ),
+                                              ),
+                                              const SizedBox(width: 12),
+                                            ],
+                                          ),
+                                          SizedBox(height: 40),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                },
+                              );
                             }
                           },
                           child: Container(
@@ -90,7 +180,8 @@ class Settings extends StatelessWidget {
                                   Transform.flip(
                                     flipX: true,
                                     child: svgViewer(
-                                        asset: "assets/svg/arrow_back.svg"),
+                                      asset: "assets/svg/arrow_back.svg",
+                                    ),
                                   ),
                               ],
                             ),
